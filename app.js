e4141de3c6c328e41fb44925d090c4ff00fe5a96
Josue Rojas <ssj52949@gmail.com>
2025-05-04 04:05:21 -0500

    agregando productos

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function consultarPorWhatsApp(producto) {
-    const telefono = '+51951012633'; // Cambia por tu número con código de país
-    const mensaje = `Hola, estoy interesado en el ${producto}`;
-    const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+function toggleDarkMode() {
+    const body = document.body;
+    const header = document.querySelector('header');
+    const boton = document.getElementById('modo-oscuro');
+    
+    // Cambia la clase 'dark-mode' solo en el cuerpo y el encabezado
+    body.classList.toggle('dark-mode');
+    header.classList.toggle('dark-mode');
+    
+    // Cambia el texto del botón dependiendo del estado
+    if (body.classList.contains('dark-mode')) {
+      boton.textContent = 'Modo Claro';
+    } else {
+      boton.textContent = 'Modo Oscuro';
+    }
+  }
+  
+  
+  function consultarPorWhatsApp(nombreProducto, precio) {
+    const mensaje = `Hola, me interesa ${nombreProducto} al precio de ${precio}`;
+    const grupoWhatsApp = 'https://chat.whatsapp.com/CHUpfk59zy8EQcpYFdHRzY'; // Enlace del grupo
+    const url = `${grupoWhatsApp}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, '_blank');
   }
   
